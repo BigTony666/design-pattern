26f345fca5725d3738c766dfeb9af99612d11368
Kejie Zhang <kejiezhang666@gmail.com>
2018-11-18 21:46:24 -0500

    feat: add extrinsic state class for flyweight pattern

--- javascript/src/structural-pattern/flyweight/Flyweight.js
+++ javascript/src/structural-pattern/flyweight/Flyweight.js
@@ -5,6 +5,8 @@
  * @keywords: Intrinsic State, Extrinsic State
  *
  * @definition: A flyweight is an object that minimizes memory usage by sharing as much data as possible with other similar objects; it is a way to use objects in large numbers when a simple repeated representation would use an unacceptable amount of memory. Often some parts of the object state can be shared, and it is common practice to hold them in external data structures and pass them to the objects temporarily when they are used.
+ *
+ * @example The String class in JDK uses this pattern
  */
 
 /**
@@ -15,8 +17,14 @@ class IgoChessman {
     throw new Error('Not implemented');
   }
 
-  display() {
-    return this.getColor();
+  display(coord) {
+    return {
+      color: this.getColor(),
+      coordinate: {
+        x: coord.x,
+        y: coord.y,
+      },
+    };
   }
 }
 
@@ -49,8 +57,34 @@ class IgoChessmanFactory {
   }
 }
 
+/**
+ * Coordinate class, an external class that will be injected into IgoChessman class, it is an extrinsic state
+ */
+class Coordinates {
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  set x(x) {
+    this._x = x;
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  set y(y) {
+    this._y = y;
+  }
+}
 export {
   BlackIgoChessman,
   WhiteIgoChessman,
   IgoChessmanFactory,
+  Coordinates,
 };
--- javascript/test/structure-pattern/flyweight/Flyweight.spec.js
+++ javascript/test/structure-pattern/flyweight/Flyweight.spec.js
@@ -1,5 +1,5 @@
 import * as chai from 'chai';
-import { IgoChessmanFactory } from '../../../src/structural-pattern/flyweight/Flyweight';
+import { IgoChessmanFactory, Coordinates } from '../../../src/structural-pattern/flyweight/Flyweight';
 
 const { assert } = chai;
 
@@ -23,8 +23,49 @@ describe('Structural-pattern ---> Flyweight', () => {
   });
 
   it('Two white chessmen are the same', () => {
-    const white1 = factory.getIgoChessman('b');
-    const white2 = factory.getIgoChessman('b');
+    const white1 = factory.getIgoChessman('w');
+    const white2 = factory.getIgoChessman('w');
     assert.deepStrictEqual(white1, white2);
+  });
+
+  it('Two different chessmen are not the same', () => {
+    const white = factory.getIgoChessman('w');
+    const black = factory.getIgoChessman('b');
+    assert.notStrictEqual(black, white);
+  });
+
+  it('Display color and coordinate', function () {
+    const black1 = factory.getIgoChessman('b');
+    const black2 = factory.getIgoChessman('b');
+    const white1 = factory.getIgoChessman('w');
+    const white2 = factory.getIgoChessman('w');
+    assert.deepStrictEqual(black1.display(new Coordinates(1, 2)), {
+      color: 'black',
+      coordinate: {
+        x: 1,
+        y: 2,
+      },
+    });
+    assert.deepStrictEqual(black2.display(new Coordinates(3, 4)), {
+      color: 'black',
+      coordinate: {
+        x: 3,
+        y: 4,
+      },
+    });
+    assert.deepStrictEqual(white1.display(new Coordinates(5, 6)), {
+      color: 'white',
+      coordinate: {
+        x: 5,
+        y: 6,
+      },
+    });
+    assert.deepStrictEqual(white2.display(new Coordinates(7, 8)), {
+      color: 'white',
+      coordinate: {
+        x: 7,
+        y: 8,
+      },
+    });
   });
 });
